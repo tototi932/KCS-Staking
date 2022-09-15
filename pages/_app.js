c65161c91aac0633865e7337e1a2ac96c6138793
NC Solutions <112722281+NewConceptSolutions@users.noreply.github.com>
2022-09-15 15:23:55 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
   return(<>    
 
 <Head>
-<title>Fusion Protocol</title>
+<title>King Charles Spaniel | Staking</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    <link
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
