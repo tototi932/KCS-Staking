cd77fde94f990ec59d1d82b3551fcd6629236768
Doge Gold Floki Finance <newcsolutions22@gmail.com>
2022-09-15 14:41:49 -0300

    Commit

---
+++ utility/stakingpools.js
@@ -0,0 +1,36 @@
+ const stakingpools = [
+    { 
+        name: "Silver Fusion",
+        image: "/img/silver.png",
+        duration: 30,
+        roi: "70%",
+        min_deposit: 20000,
+        poolId: 0
+    },
+    { 
+        name: "Gold Fusion",
+        image: "/img/gold.png",
+        duration: 60,
+        roi: "90%",
+        min_deposit: 60000,
+        poolId: 1
+    },
+    { 
+        name: "Platinum Fusion",
+        image: "/img/blue.png",
+        duration: 90,
+        roi: "120%",
+        min_deposit: 100000,
+        poolId: 2
+    },
+    { 
+        name: "Mithril Fusion",
+        image: "/img/pink.png",
+        duration: 120,
+        roi: "150%",
+        min_deposit: 200000,
+        poolId: 3
+    },
+];
+
+export default stakingpools;
