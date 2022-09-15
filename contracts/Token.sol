8685faa946e30a95654506aa1c8b77919e59dcf8
NC Solutions <112722281+NewConceptSolutions@users.noreply.github.com>
2022-09-15 14:50:35 -0300

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract testToken is ERC20{
+contract testToken is BEP20{
 
     constructor(string memory _name, string memory _symbol) ERC20( _name, _symbol){
 
