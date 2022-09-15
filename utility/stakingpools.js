f47c3e9700f415ba502fa79b99182cc4d9e5dd50
NC Solutions <112722281+NewConceptSolutions@users.noreply.github.com>
2022-09-15 15:36:35 -0300

    Update stakingpools.js

--- utility/stakingpools.js
+++ utility/stakingpools.js
@@ -1,34 +1,34 @@
  const stakingpools = [
     { 
-        name: "Silver Fusion",
+        name: "Staking Silver",
         image: "/img/silver.png",
         duration: 30,
-        roi: "70%",
-        min_deposit: 20000,
+        roi: "10%",
+        min_deposit: 30.000,
         poolId: 0
     },
     { 
-        name: "Gold Fusion",
+        name: "Staking Gold",
         image: "/img/gold.png",
         duration: 60,
-        roi: "90%",
-        min_deposit: 60000,
+        roi: "20%",
+        min_deposit: 60.000,
         poolId: 1
     },
     { 
-        name: "Platinum Fusion",
+        name: "Staking Platinum",
         image: "/img/blue.png",
         duration: 90,
-        roi: "120%",
-        min_deposit: 100000,
+        roi: "30%",
+        min_deposit: 100.000,
         poolId: 2
     },
     { 
-        name: "Mithril Fusion",
+        name: "Staking Diamond",
         image: "/img/pink.png",
         duration: 120,
-        roi: "150%",
-        min_deposit: 200000,
+        roi: "40%",
+        min_deposit: 200.000,
         poolId: 3
     },
 ];
